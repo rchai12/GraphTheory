2a3417094f7b566db766e0c4b63fdcb6e69b848d
Senait747 <sanymelak04@gmail.com>
2025-04-17 00:04:02 -0400

    Update bellmanford.py

--- bellmanford.py
+++ bellmanford.py
@@ -2,21 +2,35 @@ class BellmanFord:
     def __init__(self, graph):
         self.graph = graph
 
-    def traverse(self, start_node):
-        distance = {node: float('inf') for node in self.graph.get_nodes()}
-        distance[start_node] = 0
-        steps = [('node', start_node)]
+    def run(self, start_node):
+        steps = []
+        distances = {node.id: float('inf') for node in self.graph.nodes}
+        distances[start_node.id] = 0
 
-        for _ in range(len(self.graph.get_nodes()) - 1):
-            for u, v, w in self.graph.get_edges():
-                if distance[u] + w < distance[v]:
-                    distance[v] = distance[u] + w
-                    steps.append(('edge', (u, v)))
+        steps.append(('node', start_node))
+
+        for _ in range(len(self.graph.nodes) - 1):
+            for edge in self.graph.edges:
+                u, v = edge.node1, edge.node2
+                weight = edge.weight
+
+                # Relax the edge
+                if distances[u.id] + weight < distances[v.id]:
+                    distances[v.id] = distances[u.id] + weight
                     steps.append(('node', v))
+                    steps.append(('edge', (u, v)))
 
-        # Optional: Detect negative-weight cycles
-        for u, v, w in self.graph.get_edges():
-            if distance[u] + w < distance[v]:
-                raise ValueError("Graph contains a negative-weight cycle")
+                if edge.directed and distances[v.id] + weight < distances[u.id]:
+                    distances[u.id] = distances[v.id] + weight
+                    steps.append(('node', u))
+                    steps.append(('edge', (v, u)))
 
-        return steps
+        # Check for negative cycles
+        for edge in self.graph.edges:
+            u, v = edge.node1, edge.node2
+            weight = edge.weight
+            if distances[u.id] + weight < distances[v.id] or \
+               (edge.directed and distances[v.id] + weight < distances[u.id]):
+                raise ValueError("Graph contains a negative weight cycle")
+
+        return steps, distances
